fe46f9b1e67f2125de9bf9ff6255a78f3ee52cb5
Michael Zavattaro <mwzavattaro@icloud.com>
2023-12-30 15:03:25 +1100

    Add basic file upload functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import Link from 'next/link';
+import { Group } from '@mantine/core';
+import { Welcome } from './components/Welcome/Welcome';
+import { ColorSchemeToggle } from './components/ColorSchemeToggle/ColorSchemeToggle';
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+
+      <Group justify="center" mt="xl">
+        <Link href="/upload">
+          <span>Upload</span>
+        </Link>
+      </Group>
     </>
   );
 }
