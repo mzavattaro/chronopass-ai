c079948dd1348ea89709e0cbd40f37b33453fdd2
Michael Zavattaro <mwzavattaro@icloud.com>
2023-12-31 11:05:22 +1100

    Add basic navigation

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,12 +1,21 @@
 import { Group } from '@mantine/core';
+import Link from 'next/link';
 import { NextPage } from 'next';
 import React from 'react';
 import UploadForm from '@/app/components/UploadForm/UploadForm';
+import styles from './Page.module.css';
 
 const Page: NextPage = () => (
-  <Group justify="center" mt="xl">
-    <UploadForm />
-  </Group>
+  <div className={styles.page}>
+    <Group justify="flex-start" mt="sm">
+      <Link href="/">
+        <span>Home</span>
+      </Link>
+    </Group>
+    <Group justify="center" mt="xl">
+      <UploadForm />
+    </Group>
+  </div>
 );
 
 export default Page;
